b371722e1dbaa83bc23bbe3f3e4b1f80bc12f0d9
sakshamkataria <sakshamkataria@outlook.com>
2023-11-23 17:04:14 +0530

    Added two files, with variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 12321;
+let accountEmail = "sakshamkataria2@gmail.com";
+var accountPassword = "1245";
+accountCity = "Jaipur";
+
+// accountId = 11;
+
+accountCity = "Bengaluru";
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello"); This statement does not work in node, only in Browsers
+
+// number => 2 to the power 53
+// bigint => for very large values of int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+
+// object 
