e83e99de8e01903afc9a4973757cbbe361b8f666
sakshamkataria <sakshamkataria@outlook.com>
2023-11-26 17:19:17 +0530

    Added Operations file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,29 @@
-let score = "33";
-console.log(typeof score);
+// let score = true;
+// // console.log(typeof score);
 
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
+  
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ************Operations************
+
+let value = 3;
+let negValue = -value;
+
+let str1 = " hello";
+let str2 = " saksham";
+
+let str3 = str2 + str1;
+// console.log(str3);
+
+console.log("1" + 2); //->12
+console.log(1 + "2"); //->12
+console.log("1" + 2 + 2); //->122
+console.log(1 + 2 + "2"); //->32
